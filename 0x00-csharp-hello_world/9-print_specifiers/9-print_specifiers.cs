8cd0c8d9601fa70c08508e7aac610b446a1aa37f
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-12 11:55:41 +0100

    First commit

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double percent = .7553;
+        double currency = 98765.4321;
+        // YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE.
+    }
+}
