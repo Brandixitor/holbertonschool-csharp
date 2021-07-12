ab69c2aeeda0afb11c7a524bfda74f6faf11a18b
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-12 11:47:08 +0100

    Update

--- 0x00-csharp-hello_world/7-concat/7-concat.cs
+++ 0x00-csharp-hello_world/7-concat/7-concat.cs
@@ -6,7 +6,7 @@ class Program
     {
         string str1 = "Holberton";
         string str2 = "School";
-        // YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE.
+        str1 = str1 + str2;
         Console.WriteLine("Welcome to {0}!", str1);
     }
 }
