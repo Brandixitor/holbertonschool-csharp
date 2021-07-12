be4f8700d81907929b8ca3c7cf397edda525bec1
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-12 13:48:12 +0100

    Update

---
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _100_hello_error
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
