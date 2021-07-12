c076a9f93aeb6be77cc5646c56e1586b8704241e
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-12 10:46:08 +0100

    First commit

---
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3_writeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
