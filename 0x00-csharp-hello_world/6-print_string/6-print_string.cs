a8f15f17eb540a038e5444652ff4399451c9eac0
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-12 11:30:39 +0100

    First commit

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "Holberton School";
+        // YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE.
+    }
+}
