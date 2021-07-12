a0d8dbad027b48050ea4088b6c82decf64ae20b4
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-12 13:19:35 +0100

    Update

--- 0x00-csharp-hello_world/8-print_variables/8-print_variables.cs
+++ 0x00-csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -7,6 +7,6 @@ class Program
         string str1 = "Street";
         string str2 = "Mission";
         int int1 = 972;
-        Console.WriteLine(int1 + " " + str1 + " " + str2);
+        Console.WriteLine(int1 + " " + str2 + " " + str1);
     }
 }
