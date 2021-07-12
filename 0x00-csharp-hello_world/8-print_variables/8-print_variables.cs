6d66b5ebe4b5c691e04d9a435b7fbf5b88f67a1e
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-12 11:50:05 +0100

    First commit

---
+++ 0x00-csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "Street";
+        string str2 = "Mission";
+        int int1 = 972;
+        // YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE.
+    }
+}
