dc62e88fb49ecafd6c14ee12607c1e3202f82a34
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-12 12:08:24 +0100

    Update

--- 0x00-csharp-hello_world/2-run_new_project.sh
+++ 0x00-csharp-hello_world/2-run_new_project.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Bash script that initializes, build and run a new C# project 
+# Bash script that initializes, builds and runs a new C# project.
 dotnet new console -o 2-new_project
 dotnet build 2-new_project 
 dotnet run --project 2-new_project/2-new_project.csproj
