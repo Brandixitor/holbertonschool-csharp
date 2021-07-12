17b5d87e2c442f06ae30121a8ff6c947da08ce5b
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-12 12:28:15 +0100

    Update

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -6,7 +6,7 @@ namespace _3_writeline
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+            Console.WriteLine(@"""Programming is like building a multilingual puzzle");
         }
     }
 }
