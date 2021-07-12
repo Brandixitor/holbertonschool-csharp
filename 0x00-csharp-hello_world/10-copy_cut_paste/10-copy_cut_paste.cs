2da24a8169bc69514da0d8e0d05132e523897151
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-12 13:26:38 +0100

    Update

--- 0x00-csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
+++ 0x00-csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
-namespace _10_copy_cut_paste
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        string word = "Holberton";
+        var word_first_3 = word.Substring(0, 3);
+        var word_last_2 = word.Substring(word.Length - 2, 2);
+        var middle_word = word.Substring(1, word.Length - 2);
+        Console.WriteLine("First 3 letters: {0}", word_first_3);
+        Console.WriteLine("Last 2 letters: {0}", word_last_2);
+        Console.WriteLine("Middle word: {0}", middle_word);
     }
 }
