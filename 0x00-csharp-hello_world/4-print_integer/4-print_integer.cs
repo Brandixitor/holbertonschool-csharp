b0796d1260a8cc032cb15c790a74767b11179d1d
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-12 10:56:20 +0100

    Update

--- 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
+++ 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -5,6 +5,6 @@ class Program
     static void Main(string[] args)
     {
         int number = 972;
-        // YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE.
+        Console.WriteLine("{0} , Mission Street", number);
     }
 }
