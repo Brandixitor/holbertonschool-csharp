b26c45fea5cde91b99f5567011e7dcd852aad5ec
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-12 11:41:49 +0100

    First commit

---
+++ 0x00-csharp-hello_world/7-concat/7-concat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "Holberton";
+        string str2 = "School";
+        // YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE.
+        Console.WriteLine("Welcome to {0}!", str1);
+    }
+}
