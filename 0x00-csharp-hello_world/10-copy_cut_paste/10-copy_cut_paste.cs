28d5bac381b3f48a034c15c512edd36523cb408f
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-12 13:23:28 +0100

    Update

---
+++ 0x00-csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10_copy_cut_paste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
