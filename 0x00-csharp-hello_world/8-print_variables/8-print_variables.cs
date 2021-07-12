c3e7487d327323be8d145d5ef8b1ec989657d7d4
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-12 13:45:51 +0100

    Update

--- 0x00-csharp-hello_world/8-print_variables/8-print_variables.cs
+++ 0x00-csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -1,14 +1,12 @@
-﻿
-using System;
+﻿using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string word = "Holberton";
-        // YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-        Console.WriteLine("First 3 letters: {0}", word_first_3);
-        Console.WriteLine("Last 2 letters: {0}", word_last_2);
-        Console.WriteLine("Middle word: {0}", middle_word);
+        string str1 = "Street";
+        string str2 = "Mission";
+        int int1 = 972;
+        Console.WriteLine(int1 + " " + str2 + " " + str1);
     }
 }
