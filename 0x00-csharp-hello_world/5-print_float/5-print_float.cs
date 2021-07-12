7339e351d42fb8040b4e13b44fc6b0b0fb383612
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-12 11:19:45 +0100

    First commit

--- 0x00-csharp-hello_world/5-print_float/5-print_float.cs
+++ 0x00-csharp-hello_world/5-print_float/5-print_float.cs
@@ -5,6 +5,6 @@ class Program
     static void Main(string[] args)
     {
         float number = 3.14159f;
-        // YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE.
+        Console.WriteLine(number);
     }
 }
