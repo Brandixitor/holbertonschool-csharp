f9b49734f6435688f3522c04bd6b8a5dfb3f342e
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-13 16:20:07 +0100

    Update

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 'a'; i <= 'z'; i++)
+            {
+                if (i != 'q' && i != 'e')
+                {
+                    System.Console.Write((char)i);
+                }
+            }
+        }
+    }
+}
