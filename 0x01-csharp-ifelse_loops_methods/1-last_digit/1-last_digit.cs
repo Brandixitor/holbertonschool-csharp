bc167177493eb967c028fc437b963894ca13dc6f
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-13 15:29:50 +0100

    Update

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        // YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+    }
+}
