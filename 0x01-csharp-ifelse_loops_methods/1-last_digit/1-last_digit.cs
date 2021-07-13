ed3fcad348a92d51e3cc006fe662982b7346453c
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-13 15:36:44 +0100

    Update

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -6,6 +6,13 @@ class Program
     {
         Random rndm = new Random();
         int number = rndm.Next(-10000, 10000);
-        // YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+        if (number > 0)
+            Console.WriteLine("{0 % 10} is positive", number);
+        else if (number == 0)
+            Console.WriteLine("{0 % 10} is zero", number);
+        else
+            Console.WriteLine("{0 % 10} is negative", number);
     }
 }
