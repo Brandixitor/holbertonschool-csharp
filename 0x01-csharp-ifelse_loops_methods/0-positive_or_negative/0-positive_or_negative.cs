2f571ad42f0b6bed13c2d0520210f61e42af2c07
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-13 15:13:29 +0100

    First commit

---
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+        // YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+    }
+}
