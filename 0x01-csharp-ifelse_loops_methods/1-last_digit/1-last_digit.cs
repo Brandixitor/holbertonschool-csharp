a7a432ff5dbb150f27951764ffd2328ea4f84547
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-13 15:37:55 +0100

    Update

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -8,11 +8,11 @@ class Program
         int number = rndm.Next(-10000, 10000);
         Random rndm = new Random();
         int number = rndm.Next(-10, 10);
-        if (number > 0)
-            Console.WriteLine("{0 % 10} is positive", number);
+        if (number > 5)
+            Console.WriteLine("{0 % 10} and is greater than 5", number);
         else if (number == 0)
-            Console.WriteLine("{0 % 10} is zero", number);
+            Console.WriteLine("{0 % 10} and is 0", number);
         else
-            Console.WriteLine("{0 % 10} is negative", number);
+            Console.WriteLine("{0 % 10} and is less than 6 and not 0", number);
     }
 }
