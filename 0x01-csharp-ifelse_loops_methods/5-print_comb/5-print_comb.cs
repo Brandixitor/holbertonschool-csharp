cab039f22bc5ab277e13a6b44fc01d660c470f9c
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-13 17:22:14 +0100

    Update

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                string separator = ", ";
+
+                if (i == 99) separator = "\n";
+                System.Console.Write($"{i:d2}{separator}");
+
+            }
+        }
+    }
+}
