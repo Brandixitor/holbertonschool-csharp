1c42bbeb0bcf59a08976672877543ae628ea2b62
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-13 15:26:33 +0100

    Update

--- 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -7,12 +7,10 @@ class Program
         Random rndm = new Random();
         int number = rndm.Next(-10, 10);
         if (number > 0)
-         Console.WriteLine("{0} is positive", number);
-        else if
-            (number = 0)
-         Console.WriteLine("{0} is zero", number);
+            Console.WriteLine("{0} is positive", number);
+        else if (number == 0)
+            Console.WriteLine("{0} is zero", number);
         else
-         Console.WriteLine("{0} is negative", number);
-
+            Console.WriteLine("{0} is negative", number);
     }
 }
