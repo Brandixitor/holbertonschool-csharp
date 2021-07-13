bb0c3833c0fed23fa8851dafcaf22cba33d735b2
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-13 16:02:18 +0100

    First commit

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 97; i <= 122; i++)
+            {
+                if (i != 113 && i != 101)
+                    Console.Write("{0}", Convert.ToChar(i));
+            }
+        }
+    }
+}
