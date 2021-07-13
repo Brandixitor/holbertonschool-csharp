5c69c8a09908ee7028a08e1eadca0a41d322487a
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-13 16:49:33 +0100

    First commit

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 99; i++)
+            {
+                string hex = i.ToString("x");
+                Console.WriteLine($"{i} = 0x{hex}");
+            }
+        }
+    }
+}
