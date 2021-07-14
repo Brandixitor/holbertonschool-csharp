3227b321756e5c49725e2dfb900316fb5c17ae58
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-14 15:37:42 +0100

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Array
+{
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        if (index < 0 || index > array.Length - 1)
+        {
+            Console.WriteLine("Index out of range");
+            return array;
+        }
+        array[index] = n;
+        return array;
+    }
+}
