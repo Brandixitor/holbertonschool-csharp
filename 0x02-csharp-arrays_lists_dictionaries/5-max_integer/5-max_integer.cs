acab49e35e3e6b13d56aeb706c58b79b9c567545
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-14 17:13:24 +0100

    First commit

--- 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 
     class List
-    {
-        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+    public static int MaxInteger(List<int> myList)
+    {
+            if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return -1;
         }
+        int max = myList[0];
+        foreach (int element in myList)
+        {
+            if (element > max)
+            {
+                max = element
+            }
+        }
+        return max;
     }
+        }
