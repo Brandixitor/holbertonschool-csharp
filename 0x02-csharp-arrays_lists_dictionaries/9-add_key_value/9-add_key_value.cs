ce37dfef048f7fcc9a6f3cf6e3049f7255fdb9aa
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-14 19:56:13 +0100

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value){
+        myDict[key] = value;
+        return myDict;
+    }
+}
