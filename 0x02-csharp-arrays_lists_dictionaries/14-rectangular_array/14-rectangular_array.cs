b5ddf5958d58232355954d76d3a09e662c1acd4f
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-14 20:17:05 +0100

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] multiDim = new int[6, 5];
+        multiDim[2,2] = 1;
+        int x = 0;
+        var output = new StringBuilder();
+        foreach (var i in multiDim)
+        {
+            if (x == 5){
+                Console.WriteLine(output);
+                output.Clear();
+                x = 0;
+            }
+            output.Append(i);
+            if (x != 4)
+                output.Append(" ");
+            x += 1;
+        }
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        for (int i = 0; i < myMatrix.GetLength(0); i++){
+            for (int b = 0; b < myMatrix.GetLength(1); b++){
+                myMatrix[i, b] *= myMatrix[i, b];
+            }
+        }
+        return myMatrix;
+    }
+}
