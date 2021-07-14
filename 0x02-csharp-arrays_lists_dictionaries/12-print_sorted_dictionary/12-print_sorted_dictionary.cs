98bbb434951d0bd651eeaf967ab655177d8fcb23
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-14 20:12:31 +0100

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict){
+        var myList = new List<string>();
+        var newDict = new Dictionary<string, string>();
+        
+        foreach (var pair in myDict){
+            myList.Add(pair.Key);
+        }
+        myList.Sort();
+        foreach(var key in myList){
+            Console.WriteLine($"{key}: {myDict[key]}");
+        }
+    }
+}
