17ac85738540b92ed51903bfe957c9305e7fcceb
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-14 16:32:06 +0100

    Fixed

--- 0x02-csharp-arrays_lists_dictionaries/4-print_list/Program.cs
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/Program.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Text;
+using System.Collections.Generic;
 
-
-    class Program
+class List
+{
+    public static List<int> CreatePrint(int size)
     {
-        static void Main(string[] args)
+        if (size < 0)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Size cannot be negative");
+            return null;
         }
+        var myList = new List<int>();
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < size; i++)
+        {
+            myList.Add(i);
+            output.Append(i);
+            if (i != size - 1)
+                output.Append(" ");
+        }
+        Console.WriteLine(output);
+        return myList;
     }
+}
