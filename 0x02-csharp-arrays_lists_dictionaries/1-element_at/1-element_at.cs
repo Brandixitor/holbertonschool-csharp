3d01b0f6181e3661f7cc76e1c2a5f6dbd8a9419d
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-14 14:03:40 +0100

    Fixed

--- 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-
 class Array
 {
     public static int elementAt(int[] array, int index)
     {
-        Console.WriteLine("Hello World!");
-
+        if (index < 0 || index > array.Length - 1)
+        {
+            Console.WriteLine("Index out of range");
+            return -1;
+        }
+        return array[index];
     }
 }
-
