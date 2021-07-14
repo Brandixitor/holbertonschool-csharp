7c2384143ef20d4f20f9e601286ee0f013a99718
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-14 13:07:30 +0100

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0_print_array
+{
+    class array
+    {
+        public static int[] CreatePrint(int size)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
