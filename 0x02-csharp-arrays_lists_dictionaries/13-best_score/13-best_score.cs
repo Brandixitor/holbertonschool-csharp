48dd2be8707b5b6b07f1d9d47a6979143bdf1edc
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-14 20:15:56 +0100

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList){
+        int max = int.MinValue;
+        string maxKey = "None";
+        foreach(var pair in myList){
+            if (pair.Value > max){
+                max = pair.Value;
+                maxKey = pair.Key; 
+            }
+        }
+        return maxKey;
+    }
+}
