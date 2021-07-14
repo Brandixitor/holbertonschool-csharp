09ee8baeb57009ad6758633e94076c1a4ddfb69e
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-14 19:35:53 +0100

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList){
+        var newList = new List<bool>();
+        for(int i = 0; i < myList.Count; i++){
+            if (myList[i] % 2 == 0){
+                newList.Add(true);
+            }
+            else{
+                newList.Add(false);
+            }
+        }
+        return newList;
+    }
+}
