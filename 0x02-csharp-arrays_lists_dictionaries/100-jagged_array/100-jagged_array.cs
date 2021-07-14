06f9a4d41f9d433280f543f10f0ecde68b3633bc
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-14 19:57:57 +0100

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[][] jaggerArr =
+        {
+            new int[] { 0, 1, 2, 3 },
+            new int[] { 0, 1, 2, 3, 4, 5, 6 },
+            new int[] { 0, 1 }
+        };
+
+        var output = new StringBuilder();
+        for (int i = 0; i < jaggerArr.Length; i++){
+            output.Clear();
+            for (int b = 0; b < jaggerArr[i].Length; b++){
+                output.Append(jaggerArr[i][b]);
+                if (jaggerArr[i].Length - 1 != b){
+                    output.Append(" ");
+                }
+            }
+            Console.WriteLine(output);
+        }
+
+    }
+}
