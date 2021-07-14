a01b0f64af39c42cd73aea9c83c81fc41f381eba
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-14 13:39:48 +0100

    Update

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -4,12 +4,27 @@ using System.Collections.Generic;
     class array
     {
         public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
         {
-            if (size < 0)
-            {
-                Console.WriteLine("Size cannot be negative");
-                return null;
-            }
+            Console.WriteLine("Size cannot be negative");
+            return null;
         }
+
+        int[] arr = new int[size];
+        var output = new StringBuilder();
+        for (int i = 0; i < size; i++)
+        {
+            output.Append(i);
+            if (i != size - 1)
+            {
+                output.Append(" ");
+            }
+            arr[i] = i;
+        }
+
+        Console.WriteLine(output);
+
+        return arr;
     }
 }
