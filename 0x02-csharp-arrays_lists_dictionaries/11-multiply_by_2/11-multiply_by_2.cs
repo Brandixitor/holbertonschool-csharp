1a2f7adc59349f6bfa50dfc3574bea5a75d0a85a
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-14 20:03:35 +0100

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        var newDict = new Dictionary<string, int>();
+        foreach (var pair in myDict)
+        {
+            newDict[pair.Key] = pair.Value * 2;
+        }
+        return newDict;
+    }
+}
