7b09cffce6fbd55af6fadddfc2edb6e6035e3efd
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-14 19:58:37 +0100

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
+    {
+        myDict.Remove(key);
+        return myDict;
+    }
+}
