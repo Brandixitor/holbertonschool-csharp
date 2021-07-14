f5609472ad85caecefad5cbc9afcc0ce0412ddf4
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-14 19:48:12 +0100

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict){
+        int i = 0;
+        foreach(var pair in myDict){
+            i += 1;
+        }
+        return i;
+    }
+}
