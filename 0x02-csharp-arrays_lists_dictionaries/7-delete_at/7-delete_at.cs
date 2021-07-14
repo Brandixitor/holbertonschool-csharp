4c93f8f3f3f07bad207e78875eea1aca550da7fd
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-14 19:41:33 +0100

    First commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index){
+        if (index < 0 || index > myList.Count - 1){
+            Console.WriteLine("Index is out of range");
+            return myList;
+        }
+        myList.Remove(myList[index]);
+        return myList;
+    }
+}
