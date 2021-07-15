daf50a442a0e41170e7a738dfe72fc9bece3b60b
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-15 16:13:58 +0100

    First commi

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList){
+        if (myLList.Count == 0)
+            return 0;
+        LinkedListNode<int> head = myLList.First;
+        myLList.RemoveFirst();
+        return head.Value;
+    }
+}
