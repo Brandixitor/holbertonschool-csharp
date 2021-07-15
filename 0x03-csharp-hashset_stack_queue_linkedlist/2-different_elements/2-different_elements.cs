e19ce25cb93ef5551dfca692d25747a7d42c831d
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-15 12:57:48 +0100

    Fixed

--- 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -1,25 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+
 class List
 {
-    public static List<int> DifferentElements(List<int> list1, List<int> list2)
-    {
+    public static List<int> DifferentElements(List<int> list1, List<int> list2){
         List<int> List3 = new List<int>();
-
-        foreach (var elem in list1)
-        {
-            if (!list2.Contains(elem))
-            {
+        foreach (var elem in list1){
+            if (!list2.Contains(elem)){
                 List3.Add(elem);
             }
-
-            foreach (var elem in list2)
-            {
-                if (!list1.Contains(elem))
-                {
-                    List3.Add(elem);
-                }
-            }
-            return List3;
         }
+        foreach (var elem in list2){
+            if (!list1.Contains(elem)){
+                List3.Add(elem);
+            }
+        }
+        List3.Sort();
+        return List3;
     }
+}
