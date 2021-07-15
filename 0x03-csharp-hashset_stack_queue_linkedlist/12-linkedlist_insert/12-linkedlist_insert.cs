c2f0c58b5846db729709e939033b2a7e182f17fd
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-15 16:34:35 +0100

    Update

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n){
+        LinkedListNode<int> node = myLList.First;
+        if (node.Value >= n)
+            return myLList.AddFirst(n);
+        while (node.Next != null){
+            if (node.Next.Value >= n){
+                node = myLList.AddAfter(node, n);
+                return node;
+            }
+            node = node.Next;
+        }
+        node = myLList.AddLast(n);
+        return node;
+    }
+}
