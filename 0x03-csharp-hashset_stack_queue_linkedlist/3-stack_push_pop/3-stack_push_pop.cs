aa52046c250c11e03b03429b0c146527f6a0df58
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-15 13:36:24 +0100

    Update

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 
-
-    class MyStack
-    {
+class MyStack
+{
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-            Console.WriteLine("Hello World!");
+        var item = "";
+
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+        if (aStack.Count == 0)
+        {
+            item = "Stack is empty";
         }
+        else
+        {
+            item = "Top item: " + aStack.Peek();
+        }
+        Console.WriteLine(item);
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search));
+        while (aStack.Contains(search))
+        {
+            aStack.Pop();
+        }
+        aStack.Push(newItem);
+        return aStack;
     }
+}
