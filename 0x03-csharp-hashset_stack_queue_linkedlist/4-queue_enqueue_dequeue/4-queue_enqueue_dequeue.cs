2cad63e922f38601eb32b8bad9148d2da661ec94
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-15 13:46:36 +0100

    First commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class MyQueue
+    {
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+        {
+        var message = "";
+        Console.WriteLine("Number of iteùs: {0}", aQueue.Count);
+        }
+    }
