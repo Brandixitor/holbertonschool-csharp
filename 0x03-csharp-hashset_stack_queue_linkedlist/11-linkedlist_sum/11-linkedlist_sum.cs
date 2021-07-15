82985189484691e8b419837272ef96752977b11e
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-15 16:27:09 +0100

    First commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int Sum(LinkedList<int> myLList){
+        int sum = 0;
+        foreach (var elem in myLList)
+            sum += elem;
+        return sum;
+    }
+}
