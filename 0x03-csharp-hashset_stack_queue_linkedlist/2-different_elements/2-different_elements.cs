2973cb0357791033288fa792d8d2ab17ddc30bff
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-15 12:39:27 +0100

    First commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
