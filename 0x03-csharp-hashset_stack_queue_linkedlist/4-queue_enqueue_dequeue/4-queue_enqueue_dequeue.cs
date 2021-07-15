acb7e3cdd30f842a0d5bc4d23ff20832900d5b67
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-15 14:00:28 +0100

    Fixed

--- 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -1,27 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 
-    class MyQueue
-    {
+class MyQueue
+{
     public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
-            {
-        var message = "";
-
+    {
+        string item;
         Console.WriteLine("Number of items: {0}", aQueue.Count);
         if (aQueue.Count == 0)
         {
-            message = "Queue is empty";
+            item = "Queue is empty";
         }
-            else
+        else
         {
-            message = "First item " + aQueue.Peek();
+            item = "First item: " + aQueue.Peek();
         }
-        Console.WriteLine(message);
+        Console.WriteLine(item);
+        aQueue.Enqueue(newItem);
         Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search));
         while (aQueue.Contains(search))
         {
             aQueue.Dequeue();
         }
         return aQueue;
-    }
     }
