0b5296a2f818819a1904a97c4ff23634caf27cdf
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-15 16:45:53 +0100

    Done

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index){
+        foreach(var elem in myLList){
+            if (index == 0){
+                myLList.Remove(elem);
+                break;
+            }
+            index--;
+        }
+    }
+}
