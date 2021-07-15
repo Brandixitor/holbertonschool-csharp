f1150c5b1eb56bf4ca28ade801ab2863b7740652
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-15 15:48:33 +0100

    Update

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value){
+        int index = 0;
+
+        foreach(var elem in myLList){
+            if (elem == value)
+                return index;
+            index++;
+        }
+        return -1;
+    }
+}
