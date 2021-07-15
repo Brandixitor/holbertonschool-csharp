1db74eb3bc0c524797ff44c2ad9d74df970511fb
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-15 14:14:15 +0100

    Fixed

--- 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
     class LList
     {
     public static LinkedList<int> CreatePrint(int size)
     {
-            Console.WriteLine("Hello World!");
+        LinkedList<int> myList = new LinkedList<int>();
+
+        for (int i = 0; i < size; i++)
+        {
+            myList.AddLast(i);
+            Console.WriteLine(i);
         }
+        return myList;
     }
+}
