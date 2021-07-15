8456a79d509dead49999f67e04f2401a114e1ba5
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-15 14:01:49 +0100

    Fixed

--- 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -5,17 +5,17 @@ class MyQueue
 {
     public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
     {
-        string item;
+        string topItem;
         Console.WriteLine("Number of items: {0}", aQueue.Count);
         if (aQueue.Count == 0)
         {
-            item = "Queue is empty";
+            topItem = "Queue is empty";
         }
         else
         {
-            item = "First item: " + aQueue.Peek();
+            topItem = "First item: " + aQueue.Peek();
         }
-        Console.WriteLine(item);
+        Console.WriteLine(topItem);
         aQueue.Enqueue(newItem);
         Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search));
         while (aQueue.Contains(search))
