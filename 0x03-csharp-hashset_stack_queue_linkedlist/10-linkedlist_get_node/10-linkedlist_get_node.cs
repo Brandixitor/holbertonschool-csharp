85debd1e2dfec3cfeace6ab602675138667ebad5
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-15 16:23:54 +0100

    First commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n){
+        int index = 0;
+        foreach (var elem in myLList){
+            if (index == n)
+                return elem;
+            index++;
+        }
+        return 0;
+    }
+}
