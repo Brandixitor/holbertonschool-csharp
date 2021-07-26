ae4c40d225d73b4151b20fadfa533d138157975c
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-26 20:21:15 +0100

    Done

--- 0x05-csharp-structs_enums/1-dog/1-dog.cs
+++ 0x05-csharp-structs_enums/1-dog/1-dog.cs
@@ -1,2 +1,17 @@
 ﻿using System;
 
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
