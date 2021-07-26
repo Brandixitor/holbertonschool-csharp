0efc9cd73fb02e227a62deeccb72b55f76040625
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-26 13:22:02 +0100

    Update

--- 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -5,6 +5,19 @@
     {
         public static int SafePrint(List<int> myList, int n)
         {
-            Console.WriteLine("Hello World!");
+        int i = 0;
+        try
+        {
+            while(i < n)
+            {
+                Console.WriteLine(myList[i]);
+                i++;
+            }
         }
+            catch (Exeption)
+        {
+
+        }
+    return i;
     }
+}
