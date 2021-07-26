40964e264338bec674dad94b5c6a0a1d2958f50a
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-26 20:31:19 +0100

    First commit

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+
+public struct Dog
+{
+    public Dog(string _name, float _age, string _owner, Rating _rating)
+    {
+        name = _name;
+        age = _age;
+        owner = _owner;
+        rating = _rating;
+    }
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
