94c44bb396b6ad37af6a2ff51f62fcbb1708bdb4
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-26 13:30:53 +0100

    First commit

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int res = 0;
+        try
+        {
+            res = a / b;
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Number cannot be divided by 0");
+        }
+        finally
+        {
+            Console.WriteLine("{0} / {1} = {2}", a, b, res);
+        }
+    }
+
+}
