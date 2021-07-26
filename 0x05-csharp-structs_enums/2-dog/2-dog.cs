a71c87ee882d6a18368e3360871198251cdac395
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-26 20:29:46 +0100

    Done

--- 0x05-csharp-structs_enums/2-dog/2-dog.cs
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -10,6 +10,13 @@ public enum Rating
 
 public struct Dog
 {
+    public Dog(string _name, float _age, string _owner, Rating _rating)
+    {
+        name = _name;
+        age = _age;
+        owner = _owner;
+        rating = _rating;
+    }
     public string name;
     public float age;
     public string owner;
