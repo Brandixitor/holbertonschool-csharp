c2d98216a7622a1672bdd038781103f41535bad2
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-27 12:18:14 +0100

    Fixed

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -2,9 +2,14 @@
 
 namespace Enemies
 {
-    class Zombie
+    /// <summary>
+    /// This class creates a zombie.
+    /// </summary>
+    public class Zombie
     {
+        /// <summary>Field that defines the health of zombie.</summary> 
         public int health;
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
         public Zombie()
         {
             health = 0;
