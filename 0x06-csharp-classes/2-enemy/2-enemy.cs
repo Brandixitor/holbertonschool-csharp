28438a3048f7c6d674c54677dda02b1ca2fea0a2
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-27 12:28:07 +0100

    First commit

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// This class creates a zombie.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>Field that defines the health of zombie.</summary> 
+        public int health;
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                Console.WriteLine("Health must be greater than or equal to 0");
+            }
+        }
+    }
+}
