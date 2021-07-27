40a900f97fcfd3e2c8df3a0f41f718d1f714b565
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-27 12:14:04 +0100

    Updated XML

--- 0x06-csharp-classes/0-enemy/0-enemy.cs
+++ 0x06-csharp-classes/0-enemy/0-enemy.cs
@@ -4,8 +4,8 @@ namespace Enemies
 {
     // This class is empty.
     /// <summary>
-    /// // This class is empty.
-    /// <summary>
+    /// This class is empty.
+    /// </summary>
     class Zombie
     {
 
